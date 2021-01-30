8e426157613351f9b8ba5e31512654dc1ecc9b4f
imander <andrsn84@gmail.com>
2021-01-30 14:30:40 -0800

    Add support for GPU instances

--- setup_aws/modules/instance/bootstrap.sh
+++ setup_aws/modules/instance/bootstrap.sh
@@ -3,6 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Add nvidia-docker repo
+source /etc/os-release
+distribution="$ID$VERSION_ID"
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
 # Install dependencies
 apt-get update
 apt-get install -y \
@@ -10,23 +16,52 @@ apt-get install -y \
   docker.io \
   docker-compose \
   jq \
+  nvidia-docker2 \
   s3fs
 
-### Ensure data volumne is fomatted and mounted
+### Ensure data volume is formatted and mounted
 mkdir -pm 777 /data
-lsblk /dev/nvme1n1 | grep -q ext4 ||
-  (
-    mkfs.ext4 /dev/nvme1n1
-    echo '/dev/nvme1n1  /data ext4  defaults,noatime  0 0' >>/etc/fstab
-  )
+device=$(lsblk -o NAME,FSTYPE -dpsn | awk '$2 == "" {print $1}')
+if [[ ! -z "$device" ]]; then
+  mkfs.ext4 $device
+  echo "$device  /data ext4  defaults,noatime  0 0" >>/etc/fstab
+  mount $device /data
+fi
 
-mount /dev/nvme1n1 /data
-
-### Change docker data location
+### Change docker data location and add runtimes
 systemctl stop docker.service
-echo '{"data-root": "/data"}' >/etc/docker/daemon.json
+
+cat <<EOF >/etc/docker/daemon.json
+{
+  "data-root": "/data",
+  "runtimes": {
+    "nvidia": {
+      "path": "nvidia-container-runtime",
+      "runtimeArgs": []
+    }
+  }
+}
+EOF
+
 systemctl start docker.service
 usermod -a -G docker ubuntu
+
+# install Nvidia drivers if running on GPU
+if lshw | grep -qi nvidia; then
+  apt-get install linux-headers-$(uname -r)
+  distribution=$(echo $distribution | sed -e 's/\.//g')
+  wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+  mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+  apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+  echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+  apt-get update
+  apt-get -y install cuda-drivers
+  export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
+  echo 'export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}' >>/home/ubuntu/.profile
+  /usr/bin/nvidia-persistenced --verbose
+  # test that nvidia docker is working
+  docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+fi
 
 ### Mount S3
 mkdir -pm 777 /mnt/irrigation_data
