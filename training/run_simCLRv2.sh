d821529d145ddf856c645888941e2ba08e7fde43
Sirak Ghebremusse <sirak.ghebrem@gmail.com>
2021-03-14 16:37:33 -0700

    BigEarthNet SimCLR train script

---
+++ training/run_simCLRv2.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DATA_DIR=/data
+ARCH=ResNet152
+OUTPUT_PREFIX="BigEarthNet_SimCLR_pretrain_${ARCH}_E50_B32"
+OUTPUT_DIR=/data/models
+EPOCHS=50
+BATCH_SIZE=32
+
+TRAIN_DATA="/data/Tfrecord.SimCLR/train.tfrecord"
+
+GREEN='\034[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+function log() {
+  msg=${1:-}
+  printf "$(date -u) ${GREEN}INFO${NC} $msg\n"
+}
+
+function warn() {
+  msg=${1:-}
+  printf "$(date -u) ${YELLOW}WARNING${NC} $msg\n"
+}
+
+function remove_container() {
+  log "Removing docker container: $DOCKER_NAME"
+  docker rm -f $DOCKER_NAME 2>/dev/null || true
+}
+
+function simclr_pretrain() {
+  mkdir -pm 777 ${OUTPUT_DIR}
+  trap remove_container EXIT
+  local outfile="${OUTPUT_PREFIX}"
+
+
+  log "Running training on $TRAIN_DATA"
+  DOCKER_NAME="training_$(date '+%Y%m%d%H%M%S')"
+  docker run --gpus all --name "$DOCKER_NAME" \
+    --user $(id -u):$(id -g) \
+    -v "$(pwd):/capstone_fall20_irrigation" \
+    -v "$OUTPUT_DIR:$OUTPUT_DIR" \
+    -v "$DATA_DIR:$DATA_DIR" \
+    -w /capstone_fall20_irrigation \
+    imander/irgapp \
+    python3 simclr.py \
+    -a "$ARCH" \
+    -o "${outfile}" \
+    --output-dir "$OUTPUT_DIR" \
+    -e "$EPOCHS" \
+    -b "$BATCH_SIZE" \
+    --train-data "$TRAIN_DATA"
+  docker logs "$DOCKER_NAME" >"${OUTPUT_DIR}/${outfile}.log"
+  remove_container
+}
+
+
+simclr_pretrain
