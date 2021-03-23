49b80eff74f2ccb11ef3b5963d82323d4e6cda59
chitraagastya <chitra.agastya@berkeley.edu>
2021-03-22 20:25:13 -0400

    changes to eliminate nan from the image downloaded from EE

--- process_data/cropland_ca/split_ca_image.py
+++ process_data/cropland_ca/split_ca_image.py
@@ -12,11 +12,58 @@ BAND_NAMES = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B8A', 'B11', 'B12']
 # According to Sentinel guide all MSI data is scaled by factor of 10000
 SCALE_FACTOR = 1 #10000
 
-def prep_example(ia_list, TFRecord_writer):
+def is_any_band_invalid(img_splt):
+    for k in img_splt.keys():
+        if k.startswith('B'):
+            if (img_splt[k] < 0).any():
+                return True
+    return False
+
+def save_obj(msg, obj, filepath):
+    if obj:
+        print(msg)
+        pickle.dump(obj, open(filepath, 'wb'))
+    else:
+        print(f'object passed is null. Nothing to save')
+
+def get_images_data(ialist, invalid_images):
+    #save the list of clean images and list of bad images
+    clean_img = {}
+    bad_img = {}
+    for k in ialist.keys():
+        if k in invalid_images:
+            bad_img[k] = ialist[k]
+        else:
+            clean_img[k] = ialist[k]
+    return clean_img, bad_img
+
+def pickle_data(invalid_images, ialist, filterNan, base_path, patch):
+    if not filterNan:
+        #save the dictionary as a pickle file
+        save_obj(f'dumping the pkl file {patch}.pkl on disk',
+            ialist, f'{base_path}/{patch}.pkl')
+
+    else:
+        clean_img, bad_img = get_images_data(ialist, invalid_images)
+        # save clean images
+        save_obj(f'dumping clean images pkl file {patch}.pkl.clean on disk',
+            clean_img, f'{base_path}/{patch}.pkl.clean')
+
+        #save the list of bad images
+        save_obj(f'dumping bad images pkl file {patch}.pkl.bad on disk',
+            bad_img, f'{base_path}/{patch}.pkl.bad')
+
+def prep_example(ia_list, TFRecord_writer, filterNan):
     try:
         progress_bar = tf.keras.utils.Progbar(target=len(ia_list))
+        invalid_images = []
         #bands, original_labels, original_labels_multi_hot, patch_name):
         for idx, key in enumerate(ia_list):
+
+            if is_any_band_invalid(ia_list[key]):
+                invalid_images.append(key)
+                continue
+
             example = tf.train.Example(
                 features=tf.train.Features(
                     feature={
@@ -61,6 +108,8 @@ def prep_example(ia_list, TFRecord_writer):
             )
             TFRecord_writer.write(example.SerializeToString())
             progress_bar.update(idx)
+
+        return invalid_images
     except:
         # printing stack trace
         traceback.print_exception(*sys.exc_info())
@@ -71,7 +120,8 @@ def split_convert2TFrecord(TFRecord_writer,
     patch,
     month,
     base_path, band_names=BAND_NAMES,
-    img_dim=120, scale = SCALE_FACTOR):
+    img_dim=120, scale = SCALE_FACTOR,
+    filterNan = True):
     '''
     base_path: String - Path to folder with tif file for each spectral band
     patch: name of the image folder
@@ -80,11 +130,9 @@ def split_convert2TFrecord(TFRecord_writer,
     scale: numeric - Value by which to divide the arrays of data in the tiff file
 
     '''
-
-    # List to store temporary dataframes
-    ia_list = {}
-
     try:
+        # List to store temporary dataframes
+        ia_list = {}
         print(f'Processing image patch {patch}')
         # Loop over all bands and read the actual MSI data in and create 3d array
         for b, band in enumerate(band_names):
@@ -110,14 +158,15 @@ def split_convert2TFrecord(TFRecord_writer,
 
 
         # Create TfRecords for the split msi
-        prep_example(ia_list, TFRecord_writer)
+        invalid_images = prep_example(ia_list, TFRecord_writer, filterNan)
+
+        #save the dictionary as a pickle file
+        pickle_data(invalid_images, ia_list, filterNan, base_path, patch)
+        #print(f'dumping the pkl file {patch}.pkl on disk')
+        #pickle.dump(ia_list, open(f'{base_path}/{patch}-clean.pkl', 'wb'))
 
     except Exception as e:
             print(f"Error: {str(e)}")
-
-    #save the dictionary as a pickle file
-    print(f'dumping the pkl file {patch}.pkl on disk')
-    pickle.dump(ia_list, open(f'{base_path}/{patch}.pkl', 'wb'))
 
 def get_month(patch):
     if patch:
@@ -130,7 +179,7 @@ def create_split(root_folder,
     #Create TFWriter for entire dataset
     try:
         print('Creating TFRecord writer')
-        tfwriter = tf.io.TFRecordWriter(os.path.join(out_folder, "CV_sample.tfrecord"))
+        tfwriter = tf.io.TFRecordWriter(os.path.join(out_folder, "CV.tfrecord"))
 
         print(f'Processing file in folder {root_folder}')
         #open the patch_names file in the folder and read it line by line to split the images
